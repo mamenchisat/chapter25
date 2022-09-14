045b4aa6d29df703a22ada7832222121b01bc2fe
17804 <896878996@qq.com>
2022-09-14 20:05:59 +0800

    Initial commit

--- src/com/hspedu/jdbc/druid/DBUtils_USE.java
+++ src/com/hspedu/jdbc/druid/DBUtils_USE.java
@@ -3,6 +3,7 @@ package com.hspedu.jdbc.druid;
 import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.handlers.BeanHandler;
 import org.apache.commons.dbutils.handlers.BeanListHandler;
+import org.apache.commons.dbutils.handlers.ScalarHandler;
 import org.testng.annotations.Test;
 
 import java.sql.Connection;
@@ -58,6 +59,28 @@ public class DBUtils_USE {
             //params给？赋值，可以有多个
             Actor actor = queryRunner.query(connection, sql, new BeanHandler<>(Actor.class), 1);
             System.out.println(actor);
+            //释放资源
+            JDBCUtilsByDruid.close(null,null,connection);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @Test
+    //返回的是单行单列对象
+    public void testQueryScalar() {
+        String sql = "select name from actor where id = ?";
+        try {
+            Connection connection = JDBCUtilsByDruid.getConnection();
+            QueryRunner queryRunner = new QueryRunner();//执行查询返回arraylist结果集
+            //1，query 方法就是执行sql语句，得到resultset 封装到arraylist集合中
+            //2.返回集合
+            //3.connection 连接
+            //sql：执行的语句
+            //new BeanListHandler<>(Actor.class)。在将resultset取出来放入actor对象，然后封装到arraylist对象，利用反射获取actor类的属性进行封装
+            //params给？赋值，可以有多个
+            Object query = queryRunner.query(connection, sql, new ScalarHandler(), 1);
+            System.out.println(query);
             //释放资源
             JDBCUtilsByDruid.close(null,null,connection);
 
