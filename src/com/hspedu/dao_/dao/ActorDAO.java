40593f4142927ff5b0edc0a844cc0d33c00989dc
Mamenchisat <896878996@qq.com>
2022-09-14 22:22:03 +0800

    Initial commit

---
+++ src/com/hspedu/dao_/dao/ActorDAO.java
@@ -0,0 +1,14 @@
+package com.hspedu.dao_.dao;
+
+import com.hspedu.dao_.domain_.Actor;
+
+/**
+ * 项目名：    chapter25
+ * 文件名：    ActorDAO
+ * 创建时间：   2022/9/14 22:20
+ *
+ * @author crazy Chen
+ * 描述：      TODO
+ */
+public class ActorDAO extends BasicDAO<Actor>{
+}
